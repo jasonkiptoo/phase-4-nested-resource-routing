0952a893e17ce7869a5b377cb42e49b9dbb0649f
JasonKiptoo <kiptoojason@gmail.com>
2022-11-30 01:36:33 +0300

    Completed assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :dog_houses, only: [:show] do
-    resources :reviews, only: [:show, :index]
 
 
+    resources :dog_houses, only: [:show] do
+      # nested resource for reviews
+      resources :reviews, only: [:show, :index]
+    end
+
+    resources :reviews, only: [:show, :index, :create]
   end
+
